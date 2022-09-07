602acf6aec9f998fe5daeafd99b8abda1e1022c7
w3cj <cj@null.computer>
2022-09-07 13:57:14 -0600

    :sparkles: dockerize express-api

--- examples/express-api/src/db.ts
+++ examples/express-api/src/db.ts
@@ -1,8 +1,14 @@
 import { MongoClient } from 'mongodb';
 
 const {
-  MONGO_URI = 'mongodb://localhost/todo-api',
+  MONGO_HOST,
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_PORT,
+  MONGO_DBNAME,
 } = process.env;
+
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}?authSource=admin`;
 
 export const client = new MongoClient(MONGO_URI);
 export const db = client.db();
