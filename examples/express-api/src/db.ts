196a27eab78211699e41cdc4cb83be7ef152fac6
w3cj <cj@null.computer>
2022-10-19 14:47:25 -0600

    :sparkles: add option for local mongo db

--- examples/express-api/src/db.ts
+++ examples/express-api/src/db.ts
@@ -6,9 +6,14 @@ const {
   MONGO_PASSWORD,
   MONGO_PORT,
   MONGO_DBNAME,
+  MONGO_LOCAL,
 } = process.env;
 
-const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}?authSource=admin`;
+let MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}?authSource=admin`;
+
+if (MONGO_LOCAL) {
+  MONGO_URI = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`;
+}
 
 export const client = new MongoClient(MONGO_URI);
 export const db = client.db();
